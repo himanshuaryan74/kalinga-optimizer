5bd1e7b3eeb5330b27174d332aaa24cda3ab5efb
himanshuaryan74 <himanshuaryan74@gmail.com>
2026-06-10 09:36:09 +0530

    Clean up imports in app.py
    Removed unused imports from app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
 
 import plotly.graph_objects as go
 import plotly.express as px
